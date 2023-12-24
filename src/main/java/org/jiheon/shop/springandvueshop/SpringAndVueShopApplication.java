ffe524b6060675b25d9a74b7361cab963849b375
jiheon2234 <jiheon2234@gmail.com>
2023-12-25 01:02:49 +0900

    SETTING(VUE)

---
+++ src/main/java/org/jiheon/shop/springandvueshop/SpringAndVueShopApplication.java
@@ -0,0 +1,13 @@
+package org.jiheon.shop.springandvueshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndVueShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndVueShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jiheon/shop/springandvueshop/SpringAndVueShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jiheon.shop.springandvueshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndVueShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
